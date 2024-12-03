31fd56126e147bcdfbe27e82558116bb8cd292de
gibbsjad5150 <gibbsjad5150@student.faytechcc.edu>
2024-12-02 22:16:12 -0500

    Add files via upload

---
+++ P1HW1_GibbsDana.py
@@ -0,0 +1,33 @@
+Python 3.13.0 (v3.13.0:60403a5409f, Oct  7 2024, 00:37:40) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Dana Gibbs-James
+>>> #December 2, 2024
+>>> #P1HW1
+>>> #Mathematical Expressions
+>>> 
+>>> # ---- Calculating Exponents ----
+... print("-----Calculating Exponents----")
+... 
+... # Get base and exponent from the user
+... base = int(input("Enter an integer as the base value: "))
+... exponent = int(input("Enter an integer as the exponent: "))
+... 
+... # Calculate the power
+... result = base ** exponent
+... 
+... # Display the result
+... print(f"\n{base} raised to the power of {exponent} is {result} !!\n")
+... 
+... # ---- Addition and Subtraction ----
+... print("-----Addition and Subtraction----")
+... 
+... # Get three separate integers from the user
+... starting_integer = int(input("Enter a starting integer: "))
+... add_integer = int(input("Enter an integer to add: "))
+... subtract_integer = int(input("Enter an integer to subtract: "))
+... 
+... # Perform the addition and subtraction
+... sum_result = starting_integer + add_integer
+... final_result = sum_result - subtract_integer
+... 
+... # Display the results
